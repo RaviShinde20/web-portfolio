68a2e402cf27b1ce1eae3db00897d71507d9749e
SauravChavan <61910452+SauravChavan@users.noreply.github.com>
2021-06-18 14:49:54 +0530

    files

---
+++ ui/src/pages/activities.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import {
+    EuiText,
+    EuiHeaderSectionItemButton,
+    EuiIcon,
+    EuiHeaderSectionItem,
+    EuiContextMenu,
+    EuiContextMenuItem,
+    EuiContextMenuPanel,
+    EuiHeaderLinks,
+    EuiHeaderLink,
+} from '@elastic/eui';
+class Activities extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            position: 'static',
+            isPopoverOpen: false
+        }
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export { Activities }
